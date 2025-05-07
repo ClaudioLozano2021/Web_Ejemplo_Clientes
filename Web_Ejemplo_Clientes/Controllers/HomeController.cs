1a00d0eae08c2a512225f82c8bf3510d2258fe4c
claud <claud@MSI>
2025-05-07 23:46:13 +0200

    Agregar archivos de proyecto.

---
+++ Web_Ejemplo_Clientes/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Web_Ejemplo_Clientes.Models;
+
+namespace Web_Ejemplo_Clientes.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Web_Ejemplo_Clientes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_Ejemplo_Clientes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
